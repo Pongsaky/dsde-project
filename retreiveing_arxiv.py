6fc6fda32412c4de4ddef3cdc75b48f28243e962
PuppyDoo <peemp5802@gmail.com>
2024-12-04 00:39:35 +0700

    Update : A bit details in retriving data script

--- retreiveing_arxiv.py
+++ retreiveing_arxiv.py
@@ -5,14 +5,23 @@ all_chunks = []
 df_iterator = pd.read_json("arxiv-metadata-oai-snapshot.json", lines=True, chunksize=500000)
 
 for i, chunk in enumerate(df_iterator):
-    selected_chunk = chunk[["id", "submitter", "authors", "title", "journal-ref", "categories", "abstract", "update_date"]]
+    selected_chunk = chunk[["id", "authors", "title", "journal-ref", "categories", "abstract", "update_date"]]
+    selected_chunk = selected_chunk.rename(columns={"journal-ref": "references", "categories" : "category", "update_date" : "year"})
     all_chunks.append(selected_chunk)
+
     print(f"Processed chunk {i}")
 
 for i in range(6):
-    print(f"Chunk number is {i}")
-    print(all_chunks[i]['journal-ref'].isnull().sum()/ all_chunks[i].shape[0])
     all_chunks[i] = all_chunks[i].dropna()
 
+for i in range(6):
+    print(f"Chunk number is {i}")
+    # display(chunk.head(5))
+    print(all_chunks[i].shape)
+    
 combined_df = pd.concat(all_chunks, ignore_index=True)
-combined_df.to_csv('ref_null_droped_Arxiv.csv', index=False)
+
+combined_df.insert(7, "source", "arxiv") 
+combined_df['year'] = combined_df['year'].str[:4]
+combined_df.to_csv('cleaned_arxiv.csv', index=False)
+
