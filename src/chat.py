56674e84582c3c1b0c0f0647262be009259a46ca
Pongsaky <pongsakon.kaew@gmail.com>
2024-12-07 14:28:50 +0700

    feat: Add DetectAdditionalData model and integrate into chat logic

--- src/chat.py
+++ src/chat.py
@@ -11,6 +11,7 @@ from langgraph.graph import START, MessagesState, StateGraph
 
 from app.models.APIResponse import APIResponse
 from app.models.UserInput import UserInput
+from app.models.DetectAdditionalData import DetectAdditionalData
 
 from dotenv import load_dotenv
 from src.interface.ChatInterface import ChatInterface  # Updated import
@@ -60,11 +61,11 @@ class Chat(ChatInterface):
         user_message = user_input.message
         chat_template_prompt = self.get_chat_template_prompt()
 
-        # TODO Retrieve the paper data from the vectorDB and format it as a message
+        # Retrieve the paper data from the vectorDB and format it as a message
         search_result = self.qdrant_clinet.get_search_results(self.collection_name, user_message, top_k=8)
         nodes : List[Node] = self.qdrant_clinet.get_paper_info(search_result=search_result)
         paper_data = self.format_nodes_to_text(nodes)
-        input_messages = chat_template_prompt.format_messages(paper_data=paper_data)
+        input_messages = chat_template_prompt.format_messages(paper_data=paper_data, message=user_message)
 
         chat_id = None
         if user_input.chat_id is not None:
@@ -77,21 +78,22 @@ class Chat(ChatInterface):
         output = self.app.invoke({"messages": input_messages}, config=config)
         print(output["messages"])
 
-        # TODO Convert JSON output to message and GraphData
+        # Convert JSON output to message and GraphData
+        graph_data, summation =  self.format_chat_json_to_graph_link(output["messages"][-1].content)
 
         return APIResponse(
-            chat_id=chat_id, message=output["messages"][-1], newGraph=None
+            chat_id=chat_id, message=summation, newGraph=graph_data
         )
     
     def test_initial_chat(self, user_input: UserInput):
         user_message = user_input.message
         chat_template_prompt = self.get_chat_template_prompt()
 
-        # TODO Retrieve the paper data from the vectorDB and format it as a message
+        # Retrieve the paper data from the vectorDB and format it as a message
         search_result = self.qdrant_clinet.get_search_results(self.collection_name, user_message, top_k=8)
         nodes : List[Node] = self.qdrant_clinet.get_paper_info(search_result=search_result)
         paper_data = self.format_nodes_to_text(nodes)
-        input_messages = chat_template_prompt.format_messages(paper_data=paper_data)
+        input_messages = chat_template_prompt.format_messages(paper_data=paper_data, message=user_message)
 
         chat_id = None
         if user_input.chat_id is not None:
@@ -112,93 +114,114 @@ class Chat(ChatInterface):
         config = {"configurable": {"thread_id": user_input.chat_id}}
         user_message = user_input.message
 
-        # TODO Use LLM to detect does it require additional data from vectorDB
+        # Use LLM to detect does it require additional data from vectorDB
         is_additional_data_required = self.detect_additional_data(user_input)
 
         input_messages = None
-        if is_additional_data_required:
-            # TODO Retrieve the paper data from the vectorDB and format it as a message
+        if is_additional_data_required.isNeed:
+            # Retrieve the paper data from the vectorDB and format it as a message
             search_result = self.qdrant_clinet.get_search_results(self.collection_name, user_message, top_k=10)
             nodes : List[Node] = self.qdrant_clinet.get_paper_info(search_result=search_result)
             paper_data = self.format_nodes_to_text(nodes)
             chat_template_prompt = self.get_chat_template_prompt()
-            input_messages = chat_template_prompt.format_messages(paper_data=paper_data)
+            input_messages = chat_template_prompt.format_messages(paper_data=paper_data, message=user_message)
         else :
+            self.set_chat_template_prompt(self.chat_template_prompt_text.replace("{paper_data}", ""))
             chat_template_prompt = self.get_chat_template_prompt()
-            input_messages = chat_template_prompt.format_messages()
+            input_messages = chat_template_prompt.format_messages(message=user_message)
 
         output = self.app.invoke({"messages": input_messages}, config=config)
 
-        # TODO Convert JSON output to GraphData
+        if self.chat_template_prompt_text.find("{paper_data}") == -1:
+            chat_template_text = "".join([message.content for message in chat_template_prompt.format_messages(message=user_message)]) + "\n{paper_data}"
+            self.set_chat_template_prompt(chat_template_text)
+
+        # Convert JSON output to GraphData
+        graph_data, summation =  self.format_chat_json_to_graph_link(output["messages"][-1].content)
 
         return APIResponse(
-            chat_id=user_input.chat_id, message=output["messages"][-1], newGraph=None
+            chat_id=user_input.chat_id, message=summation, newGraph=graph_data
         )
     
     def test_continue_chat(self, user_input: UserInput):
         config = {"configurable": {"thread_id": user_input.chat_id}}
         user_message = user_input.message
 
-        # TODO Use LLM to detect does it require additional data from vectorDB
+        # Use LLM to detect does it require additional data from vectorDB
         is_additional_data_required = self.detect_additional_data(user_input)
         print(is_additional_data_required)
 
         input_messages = None
-        if is_additional_data_required:
-            # TODO Retrieve the paper data from the vectorDB and format it as a message
+        if is_additional_data_required.isNeed:
+            # Retrieve the paper data from the vectorDB and format it as a message
             search_result = self.qdrant_clinet.get_search_results(self.collection_name, user_message, top_k=10)
             nodes : List[Node] = self.qdrant_clinet.get_paper_info(search_result=search_result)
             paper_data = self.format_nodes_to_text(nodes)
             chat_template_prompt = self.get_chat_template_prompt()
-            input_messages = chat_template_prompt.format_messages(paper_data=paper_data)
+            input_messages = chat_template_prompt.format_messages(paper_data=paper_data, message=user_message)
         else :
             self.set_chat_template_prompt(self.chat_template_prompt_text.replace("{paper_data}", ""))
             chat_template_prompt = self.get_chat_template_prompt()
-            input_messages = chat_template_prompt.format_messages()
+            input_messages = chat_template_prompt.format_messages(message=user_message)
 
         output = self.app.invoke({"messages": input_messages}, config=config)
 
         if self.chat_template_prompt_text.find("{paper_data}") == -1:
-            chat_template_text = "".join([message.content for message in chat_template_prompt.format_messages()]) + "\n{paper_data}"
+            chat_template_text = "".join([message.content for message in chat_template_prompt.format_messages(message=user_message)]) + "\n{paper_data}"
             self.set_chat_template_prompt(chat_template_text)
 
         for message in output["messages"]:
             print(message.pretty_print())
 
+        # TODO Convert JSON output to GraphData
+
         return output["messages"][-1]
 
-    def detect_additional_data(self, user_input: UserInput) -> bool:
+    def detect_additional_data(self, user_input: UserInput) -> DetectAdditionalData:
         config = {"configurable": {"thread_id": user_input.chat_id}}
         user_message = user_input.message
         current_graph = user_input.currentGraph
 
-        # TODO Use LLM to detect does it require additional data from vectorDB
+        # Use LLM to detect does it require additional data from vectorDB
         detect_additional_data_template = self.get_detect_additional_data_template()
         input_messages = detect_additional_data_template.format_messages(message=user_message, current_graph=current_graph)
 
         output = self.app.invoke({"messages": input_messages}, config=config)
-
-        ## TODO Check the output message is "Yes" or "No"
+        chat_json_response = output["messages"][-1].content
         print(output["messages"][-1].content)
 
-        return self.convert_detect_additional_data_to_boolean(output["messages"][-1].content)
+        chat_json_response = chat_json_response.split("```json")[1].split("```")[0]
+        detect_additional_response_json = json.loads(chat_json_response)
+
+        detect_additional_response = DetectAdditionalData(
+            isNeed=self.convert_detect_additional_data_to_boolean(detect_additional_response_json["isNeed"]),
+            reason=detect_additional_response_json["reason"]
+        )
+
+        return detect_additional_response
     
-    def test_detect_additional_data(self, user_input: UserInput):
+    def test_detect_additional_data(self, user_input: UserInput) :
         config = {"configurable": {"thread_id": user_input.chat_id}}
         user_message = user_input.message
         current_graph = user_input.currentGraph
 
-        # TODO Use LLM to detect does it require additional data from vectorDB
+        # Use LLM to detect does it require additional data from vectorDB
         detect_additional_data_template = self.get_detect_additional_data_template()
         input_messages = detect_additional_data_template.format_messages(message=user_message, current_graph=current_graph)
 
         output = self.app.invoke({"messages": input_messages}, config=config)
-
+        chat_json_response = output["messages"][-1].content
         print(output["messages"][-1].pretty_print())
-        # for message in output["messages"]:
-        #     print(message.pretty_print())
 
-        return self.convert_detect_additional_data_to_boolean(output["messages"][-1].content)
+        chat_json_response = chat_json_response.split("```json")[1].split("```")[0]
+        detect_additional_response_json = json.loads(chat_json_response)
+
+        detect_additional_response = DetectAdditionalData(
+            isNeed=self.convert_detect_additional_data_to_boolean(detect_additional_response_json["isNeed"]),
+            reason=detect_additional_response_json["reason"]
+        )
+
+        return detect_additional_response
 
     def get_chat_history(self, chat_id: str) -> List[str]:
         config = {"configurable": {"thread_id": chat_id}}
@@ -271,7 +294,7 @@ class Chat(ChatInterface):
             paper_data += f"{node_json}\n"
         return paper_data
 
-    def format_chat_json_to_graph_link(self, chat_json_response: str) -> List[GraphLink]:
+    def format_chat_json_to_graph_link(chat_json_response: str) -> List[GraphLink]:
         """
         Example of chat_json_response
         ```json
@@ -284,9 +307,42 @@ class Chat(ChatInterface):
         ]
         ```
         """
-        links_data = json.loads(chat_json_response)
-        graph_links = [GraphLink(**link) for link in links_data]
-        return graph_links
+
+        # Select content between ```json and ```
+        chat_json_response = chat_json_response.split("```json")[1].split("```")[0]
+        # print(chat_json_response)
+        graph_data = json.loads(chat_json_response)
+        # print(graph_data)
+        nodes_json = graph_data["nodes"]
+        links_json = graph_data["links"]
+
+        nodes = []
+        for node in nodes_json:
+            if "label" in node:
+                node["title"] = node["label"]
+                del node["label"]
+            
+            if "year" not in node:
+                node["year"] = None
+            
+            if "abstract" not in node:
+                node["abstract"] = None
+
+            if "authors" not in node:
+                node["authors"] = None
+
+            if "source" not in node:
+                node["source"] = None
+                
+            node = Node(**node)
+            nodes.append(node)
+
+        links = []
+        for link in links_json:
+            link = GraphLink(**link)
+            links.append(link)
+
+        return GraphData(nodes=nodes, links=links), graph_data["summation"]
     
     def convert_detect_additional_data_to_boolean(self, detect_additional_data_response: str) -> bool:
         # normalize the detect_additional_data_response to lowercase and delete \n
